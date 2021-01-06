07740b6f02d22977d7e564296d571721bba001e7
Wandu <31390092+sMiter911@users.noreply.github.com>
2021-01-06 16:22:00 +0200

    Added backend

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses'
+import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
+
+import { GlobalProvider } from './context/GlobalState'
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header></Header>
+      <div className="container">
+        <Balance></Balance>
+        <IncomeExpenses></IncomeExpenses>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
